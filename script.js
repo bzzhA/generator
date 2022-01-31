2576cd6567eca4f4507c3c7e196c3f78ff57c334
mac <mac@macs-MacBook-Air.local>
2022-02-01 00:02:00 +0400

    main

---
+++ script.js
@@ -0,0 +1,267 @@
+let arr_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let arr_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~'];
+let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let arr_num_en = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_num_EN = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let arr_num_EN_en = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_en_EN = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let arr_num_EN_en_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_num_symb = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~'];
+let arr_en_EN_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let arr_en_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_EN_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let arr_num_en_symb = ['!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr_EN_symb_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&', '?', '-', '+', '=', '~', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+let generatorForm = document.querySelector('form');
+let clickButton = document.getElementById('copy_button');
+let generate = document.getElementById('generateButton');
+let dropmenu = document.getElementById('dropmenu');
+let clearBtn = document.getElementById('clear_button');
+let modal = document.getElementById("modalWindow");
+let clearModal = document.getElementById("close");
+
+
+let numbers = document.getElementById('checkboxNumbers');
+let symbol = document.getElementById('checkboxSymbol');
+let eng = document.getElementById('checkboxeng');
+let ENG = document.getElementById('checkboxENG');
+
+
+//Очистка формы//
+clearBtn.addEventListener('click', () => {
+  generatorForm.elements.passwords.value = ''
+})
+
+//Кнопка копирования//
+generatorForm.addEventListener('submit', copy)
+function copy(event) {
+  let text = generatorForm.elements.passwords.value
+  window.navigator.clipboard.writeText(text)
+  event.preventDefault();
+}
+
+//Символы//
+generate.addEventListener('click', getSymb)
+function getSymb() {
+  if (generatorForm.elements.symbols.checked == true) {
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rndSymb = Math.floor(Math.random() * arr_symb.length);
+      text += arr_symb[rndSymb]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Большие буквы//
+generate.addEventListener('click', getEN)
+function getEN() {
+  if (generatorForm.elements.uppercase.checked == true) {
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rndEN = Math.floor(Math.random() * arr_EN.length);
+      text += arr_EN[rndEN]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Маленькие буквы//
+generate.addEventListener('click', geten)
+function geten() {
+  if (generatorForm.elements.lowercase.checked == true) {
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnden = Math.floor(Math.random() * arr_en.length);
+      text += arr_en[rnden]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Числа//
+generate.addEventListener('click', getNum)
+function getNum() {
+  if (generatorForm.elements.numbers.checked == true) {
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rndNUM = Math.floor(Math.random() * arr_num.length);
+      text += arr_num[rndNUM]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Числа и маленькие буквы//
+generate.addEventListener('click', num_en)
+function num_en() {
+  if (generatorForm.elements.numbers.checked && generatorForm.elements.lowercase.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_num_en = Math.floor(Math.random() * arr_num_en.length);
+      text += arr_num_en[rnd_num_en]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Числа и большие буквы//
+generate.addEventListener('click', num_EN)
+function num_EN() {
+  if (generatorForm.elements.numbers.checked && generatorForm.elements.uppercase.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_num_EN = Math.floor(Math.random() * arr_num_EN.length);
+      text += arr_num_EN[rnd_num_EN]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Числа и символы//
+generate.addEventListener('click', num_symb)
+function num_symb() {
+  if (generatorForm.elements.numbers.checked && generatorForm.elements.symbols.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_num_symb = Math.floor(Math.random() * arr_num_symb.length);
+      text += arr_num_symb[rnd_num_symb]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Маленькие и большие буквы//
+generate.addEventListener('click', en_EN)
+function en_EN() {
+  if (generatorForm.elements.lowercase.checked && generatorForm.elements.uppercase.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_en_EN = Math.floor(Math.random() * arr_en_EN.length);
+      text += arr_en_EN[rnd_en_EN]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Буквы большие\малые,Символы//
+generate.addEventListener('click', en_EN_symb)
+function en_EN_symb() {
+  if (generatorForm.elements.lowercase.checked && generatorForm.elements.uppercase.checked && generatorForm.elements.symbols.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value){
+      let rnd_en_EN_symb = Math.floor(Math.random() * arr_en_EN_symb.length);
+      text += arr_en_EN_symb[rnd_en_EN_symb]
+      i++
+    } 
+    generatorForm.elements.passwords.value = text
+ }
+}
+
+//Все символы (Числа,Буквы большие\малые,Символы//
+generate.addEventListener('click', all_symb)
+function all_symb() {
+  if (generatorForm.elements.lowercase.checked && generatorForm.elements.uppercase.checked && generatorForm.elements.numbers.checked && generatorForm.elements.symbols.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value){
+      let rnd_all = Math.floor(Math.random() * arr_num_EN_en_symb.length);
+      text += arr_num_EN_en_symb[rnd_all]
+      i++
+    } 
+    generatorForm.elements.passwords.value = text
+ }
+}
+
+//Символы и маленькие буквы//
+generate.addEventListener('click', symb_en)
+function symb_en() {
+  if (generatorForm.elements.symbols.checked && generatorForm.elements.lowercase.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_symb_en = Math.floor(Math.random() * arr_en_symb.length);
+      text += arr_en_symb[rnd_symb_en]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Символы и большие буквы//
+generate.addEventListener('click', symb_EN)
+function symb_EN() {
+  if (generatorForm.elements.symbols.checked && generatorForm.elements.uppercase.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value) {
+      let rnd_symb_EN = Math.floor(Math.random() * arr_EN_symb.length);
+      text += arr_EN_symb[rnd_symb_EN]
+      i++
+    }
+    generatorForm.elements.passwords.value = text
+  }
+}
+
+//Буквы Малые,Символы и числа//
+generate.addEventListener('click', en_num_symb)
+function en_num_symb() {
+  if (generatorForm.elements.lowercase.checked && generatorForm.elements.numbers.checked && generatorForm.elements.symbols.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value){
+      let rnd_en_num_symb = Math.floor(Math.random() * arr_num_en_symb.length);
+      text += arr_num_en_symb[rnd_en_num_symb]
+      i++
+    } 
+    generatorForm.elements.passwords.value = text
+ }
+}
+
+//Буквы Большие,Символы и Числа//
+generate.addEventListener('click', EN_num_symb)
+function EN_num_symb() {
+  if (generatorForm.elements.uppercase.checked && generatorForm.elements.numbers.checked && generatorForm.elements.symbols.checked == true){
+    let text = '';
+    let i = 0;
+    while (i < generatorForm.elements.menu.value){
+      let rnd_EN_num_symb = Math.floor(Math.random() * arr_EN_symb_num.length);
+      text += arr_EN_symb_num[rnd_EN_num_symb]
+      i++
+    } 
+    generatorForm.elements.passwords.value = text
+ }
+}
+
+// //Ни один чекбокс не выбран!//
+clearModal.addEventListener('click', () => {
+  modal.style.display = "none";
+})
+
+generate.addEventListener('click', () => {
+  if (!generatorForm.elements.numbers.checked && !generatorForm.elements.lowercase.checked && !generatorForm.elements.uppercase.checked && !generatorForm.elements.symbols.checked){
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
+})
